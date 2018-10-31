376fc1c6c23312743ec19f6f822d7c22788e8c88
Wei-Cheng Wu <spencerwu85@gmail.com>
2018-10-31 16:26:32 +0800

    Sort

--- build_and_run.py
+++ build_and_run.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python2.7
 import sys
 import os
-import time
-from subprocess import Popen, PIPE
 
 
 def generate(if_num):
-    path = "s1/"
+    path = "out/"
     os.system("rm -rf " + path)
     os.system("mkdir -p " + path)
     no = 0
     while no < 10:
         target = path + "test" + str(if_num) + "_" + str(no)
-        re = os.system("./dual-generator " + target + " " + str(if_num * 5) + " " + str(if_num * 50) + " " + str(if_num))
+        re = os.system("./src/dual-generator " + target + " " + str(if_num * 5) + " " + str(if_num * 50) + " " + str(if_num))
         time.sleep(1)
         while re != 0:
-            re = os.system("./dual-generator " + target + " " + str(if_num * 5) + " " + str(if_num * 50) + " " + str(if_num))
+            re = os.system("./src/dual-generator " + target + " " + str(if_num * 5) + " " + str(if_num * 50) + " " + str(if_num))
             time.sleep(1)
         no += 1 
     
 
 def main():
+    os.system("cd src/ && make && cd ../")
     if_num = 1 
     while if_num <= 10:
         generate(if_num)
